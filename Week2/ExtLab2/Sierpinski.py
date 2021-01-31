8703a3a2246e562f26598cdfb4f2f8382906e3ac
tboonma <tboonma@hotmail.com>
2021-01-31 22:36:26 +0700

    Solve extlab2 sierpinski.py

--- Week2/ExtLab2/Sierpinski.py
+++ Week2/ExtLab2/Sierpinski.py
@@ -1,3 +1,29 @@
+import turtle
+turtle.speed(0)
 
+def draw_triangle(size, n=3):
+    if n == 0:
+        return
+    turtle.forward(size)
+    turtle.left(120)
+    draw_triangle(size, n-1)
 
+def sierpinski(n, size=200):
+    if n==0:
+        draw_triangle(size)
+    else:
+        sierpinski(n-1, size/2)
+        turtle.forward(size/2)
+        sierpinski(n-1, size/2)
+        turtle.left(60)
+        turtle.forward(size/2)
+        turtle.left(120)
+        turtle.forward(size/2)
+        turtle.left(180)
+        sierpinski(n-1, size/2)
+        turtle.right(120)
+        turtle.forward(size/2)
+        turtle.left(120)
 
+sierpinski(5)
+turtle.done()
